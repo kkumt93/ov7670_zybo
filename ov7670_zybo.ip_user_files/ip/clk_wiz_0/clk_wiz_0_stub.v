818baade29510e54bdf48fb0c377b5ec89b1aa54
kkumt93 <naniwa.k.m0903@gmail.com>
2016-12-26 00:16:22 +0900

    add

--- ov7670_zybo.ip_user_files/ip/clk_wiz_0/clk_wiz_0_stub.v
+++ ov7670_zybo.ip_user_files/ip/clk_wiz_0/clk_wiz_0_stub.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2016 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2016.2 (win64) Build 1577090 Thu Jun  2 16:32:40 MDT 2016
-// Date        : Sun Dec 25 00:08:55 2016
+// Date        : Mon Dec 26 00:12:00 2016
 // Host        : LAPTOP-LAS5OT6M running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode synth_stub
 //               C:/Users/MatsudaKazuaki/Documents/GitHub/ov7670_zybo/ov7670_zybo.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0_stub.v
